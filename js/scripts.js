31df0695e5f4f5437789bb0f5c5b2ddc3af78f9d
MaryMonari <moraamarymonari@gmail.com>
2017-04-10 19:01:38 +0300

    added form inputs: select boxes, radio buttons, date and color

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// SELECT BOXES
+var beverage = $("#beverage").val();
+
+// RADIO BUTTONS
+var flavour =$("input:radio[name=flavor]:checked").val();
+
+// DATE
+var dob =$("#born").val();
+
+// COLOR
+var favoriteColor = $("#color").val();
+   
