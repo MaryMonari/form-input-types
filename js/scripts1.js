ec03a8bb2d97f22b493b489b1ec7881a49b3ea3e
MaryMonari <moraamarymonari@gmail.com>
2017-04-20 18:52:56 +0300

    added survey form

---
+++ js/scripts1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#fill form").submit(function(){
+  var choice =$("#choice").val();
+
+     $(".firstname").val();
+     $(".secondname").val();
+
+  var ages =$("#ages").val();
+
+    
+    $("#output1").text(result);
+    event.preventDefault();
+  });
+});
